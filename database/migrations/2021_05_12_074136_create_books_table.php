22841c558c8ad20d12cd69331d9d1def6bb22efe
syuji-yamamoto <aitaiforty@yahoo.co.jp>
2021-05-12 17:02:17 +0900

    bookテーブルの作成

--- database/migrations/2021_05_12_074136_create_books_table.php
+++ database/migrations/2021_05_12_074136_create_books_table.php
@@ -14,9 +14,11 @@ class CreateBooksTable extends Migration
     public function up()
     {
         Schema::create('books', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('title');
             $table->text('contents');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
