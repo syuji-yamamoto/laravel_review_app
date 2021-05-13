d3dd20f4ff8662e8db24772f743259470f072425
syuji-yamamoto <aitaiforty@yahoo.co.jp>
2021-05-13 21:32:43 +0900

    一覧画面に投稿内容を綺麗に表示させる

--- resources/views/book/index.blade.php
+++ resources/views/book/index.blade.php
@@ -5,11 +5,24 @@
   <div class="border p-4">
     <h1 class="mb-4 font-weight-bold">投稿一覧ページ</h1>
     <a class="btn btn-primary" href="{{ url('/book/create') }}">投稿ページへ</a>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">ニックネーム</th>
+          <th scope="col">タイトル</th>
+          <th scope="col">レビュー</th>
+        </tr>
+      </thead>
+      <tbody>
       @foreach ($books as $book)
-        {{ $book->id }}
-        {{ $book->title }}
-        {{ $book->contents }}
+        <tr>
+          <td>{{ $book->id }}</td>
+          <td>{{ $book->title }}</td>
+          <td>{{ $book->contents }}</td>
+        </tr>
       @endforeach
+      </tbody>
+    </table>
   </div>
 </div>
 @endsection
