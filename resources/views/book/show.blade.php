56268f4fb065ca88b29833a9fbc1ec3970535620
syuji-yamamoto <aitaiforty@yahoo.co.jp>
2021-05-14 17:54:43 +0900

    一覧画面から詳細画面への遷移と詳細画面の作成

--- resources/views/book/show.blade.php
+++ resources/views/book/show.blade.php
@@ -3,8 +3,7 @@
 @section('content')
 <div class="container mt-4">
   <div class="border p-4">
-    <h1 class="mb-4 font-weight-bold">投稿一覧ページ</h1>
-    <a class="btn btn-primary" href="{{ url('/book/create') }}">投稿ページへ</a>
+    <h1 class="mb-4 font-weight-bold">投稿詳細ページ</h1>
     <table class="table">
       <thead>
         <tr>
@@ -12,17 +11,17 @@
           <th scope="col">タイトル</th>
           <th scope="col">レビュー</th>
           <th scope="col"></th>
+          <th scope="col"></th>
         </tr>
       </thead>
       <tbody>
-      @foreach ($books as $book)
         <tr>
           <td>{{ $book->user->nickname }}</td>
           <td>{{ $book->title }}</td>
           <td>{{ $book->contents }}</td>
-          <td><a class="btn btn-primary" href="{{ route('book.show', $book->id) }}">詳細</a></td>
+          <td><a class="btn btn-primary" href="#">編集</a></td>
+          <td><a class="btn btn-primary" href="#">削除</a></td>
         </tr>
-      @endforeach
       </tbody>
     </table>
   </div>
