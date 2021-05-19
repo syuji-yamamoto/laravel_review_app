d4d3aad6da8c6415e116d81a80755b781bf6d5f3
syuji-yamamoto <aitaiforty@yahoo.co.jp>
2021-05-19 15:15:19 +0900

    commentモデルを作成して、リレーションを設定

---
+++ app/Models/Comment.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    protected $table = 'comments';
+    protected $guarded = array('id'); # 予期せぬ代入を防ぐためのコード
+    protected $fillable = [ "comment", "book_id", "user_id" ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+
+    public function book()
+    {
+        return $this->belongsTo('App\Models\Book');
+    }
+}
