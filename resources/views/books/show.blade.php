ca6e8cbd8e70422ff0f7fa05d45cf386e91f3e3f
syuji-yamamoto <aitaiforty@yahoo.co.jp>
2021-05-19 17:41:11 +0900

    viewの見た目の修正

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -40,48 +40,50 @@
       <img src="{{ Storage::url($book->image) }}" alt="image" width="150px" height="100px">
     @endif
   </div>
-
-  @if (Auth::check())
-    <form class="mb-4" method="POST" action="{{ route('comment.store') }}">
-      @csrf
-      <input name="book_id" type="hidden" value="{{ $book->id }}">
-      <fieldset class="mb-4">
-        <div class="form-group">
-          <label for="body" style="margin-top: 10px;">コメント入力</label>
-          <textarea id="comment" name="comment" class="form-control {{ $errors->has('comment') ? 'is-invalid' : '' }}" rows="4">{{ old('comment') }}</textarea>
-            @if ($errors->has('comment'))
-              <div class="invalid-feedback">
-                {{ $errors->first('comment') }}
-              </div>
-            @endif
-        </div>
-        <div class="mt-4">
-          <button type="submit" class="btn btn-primary">
-            コメントする
-          </button>
-        </div>
-      </fieldset>
-    </form>
-  @else
-    <p class="text-center">
-      **************************************<br>
-        ログインするとコメントすることができます。<br>
-      **************************************<br>
-    </p>
-  @endif
-
-  <h1 class="font-weight-bold">コメント一覧</h1>
-  @forelse ($book->comment as $comment)
-    <div class="border-top">
-      <p>
-        投稿者：{{ $comment->user->nickname }}<br>
-        コメント：{{ $comment->comment }}
+  
+  <div class="border p-4" style="margin-top: 10px;">
+    @if (Auth::check())
+      <form class="mb-4" method="POST" action="{{ route('comment.store') }}">
+        @csrf
+        <input name="book_id" type="hidden" value="{{ $book->id }}">
+        <fieldset class="mb-4">
+          <div class="form-group">
+            <label for="comment" style="margin-top: 10px;">コメント入力</label>
+            <textarea id="comment" name="comment" class="form-control {{ $errors->has('comment') ? 'is-invalid' : '' }}" rows="4">{{ old('comment') }}</textarea>
+              @if ($errors->has('comment'))
+                <div class="invalid-feedback">
+                  {{ $errors->first('comment') }}
+                </div>
+              @endif
+          </div>
+          <div class="mt-4">
+            <button type="submit" class="btn btn-primary">
+              コメントする
+            </button>
+          </div>
+        </fieldset>
+      </form>
+    @else
+      <p class="text-center">
+        **************************************<br>
+          ログインするとコメントすることができます。<br>
+        **************************************<br>
       </p>
-    </div>
-  @empty
-    コメントはまだありません。
-  @endforelse
-  <a class="btn btn-primary" href="{{ url('/') }}">投稿一覧ページへ戻る</a>
+    @endif
+
+    <h5 class="font-weight-bold">コメント一覧</h5>
+    @forelse ($book->comment as $comment)
+      <div class="border-top">
+        <p>
+          投稿者：{{ $comment->user->nickname }}<br>
+          コメント：{{ $comment->comment }}
+        </p>
+      </div>
+    @empty
+      コメントはまだありません。
+    @endforelse
+  </div>
+  <a class="btn btn-primary" href="{{ url('/') }}" style="margin-top: 10px;">投稿一覧ページへ戻る</a>
 </div>
 
 @endsection
