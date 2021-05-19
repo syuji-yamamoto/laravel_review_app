a0cc2993017ec65865cf02d3e70004d388a20f97
syuji-yamamoto <aitaiforty@yahoo.co.jp>
2021-05-19 15:17:17 +0900

    commentリクエストを作成して、バリデーションを定義

---
+++ app/Http/Requests/CommentRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CommentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'comment' => 'required|max:350',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'comment.required' => 'コメント本文を入力してください',
+            'comment.max' => 'コメント本文は350文字以内で入力してください',
+        ];
+    }
+}
